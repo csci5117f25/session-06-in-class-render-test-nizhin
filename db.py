1018810dc0511762be191c28d20e7791c3aefaaa
Quenton Ni <ni000100@umn.edu>
2025-09-25 09:48:58 -0500

    Small bug fix

--- db.py
+++ db.py
@@ -20,7 +20,6 @@ pool = None
 def setup():
     global pool
     DATABASE_URL = os.environ['DATABASE_URL']
-    print(DATABASE_URL)
     #current_app.logger.info(f"creating db connection pool")
     pool = ThreadedConnectionPool(1, 100, dsn=DATABASE_URL, sslmode='require')
 
