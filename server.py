d76c098f49d3a6dc816b5d4228b234c23e50a417
Quenton Ni <ni000100@umn.edu>
2025-09-25 09:41:24 -0500

    add guestbook functionality

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import db
+
 
 app = Flask(__name__)
+
+db.setup()
 
 @app.route('/')
 @app.route('/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    people = db.get_people()
+    return render_template('hello.html', name=name, people=people)
+
+@app.route('/submit',methods=['post'])
+def handle_submission():
+    name = request.form['name']
+    db.add_person(name)
+    return redirect('/')
