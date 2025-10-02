c79eb1980ef95a4013dfa83be688e8e16734cee3
Quenton Ni <ni000100@umn.edu>
2025-10-02 09:37:30 -0500

    add auth

--- server.py
+++ server.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for
+from functools import wraps
 import db
-
+from urllib.parse import quote_plus, urlencode
+from dotenv import find_dotenv, load_dotenv
+from authlib.integrations.flask_client import OAuth
+from os import environ as env
 
 app = Flask(__name__)
 
 db.setup()
 
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app.secret_key = env.get("APP_SECRET_KEY")
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'user' not in session:
+      # Redirect to Login page here
+      return redirect('/login')
+    return f(*args, **kwargs) #do the normal behavior -- return as it does.
+  return decorated
+
 @app.route('/')
 @app.route('/<name>')
+@requires_auth
 def hello(name=None):
     people = db.get_people()
     return render_template('hello.html', name=name, people=people)
@@ -17,3 +49,31 @@ def handle_submission():
     name = request.form['name']
     db.add_person(name)
     return redirect('/')
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://" + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("hello", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
